7a6c57591d1ba3eb95925d4ed9bc05bcf0e1f6e0
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-04-07 21:44:21 -0600

    Add return and revenue methods to dock class

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,14 +1,16 @@
 class Dock
-  attr_reader :name, :max_rental_time, :rental_log
+  attr_reader :name, :max_rental_time, :rental_log, :revenue
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
     @rental_log = {}
+    @revenue = 0
   end
 
   def rent(boat, renter)
-    rental_log[boat] = renter
+    @rental_log[boat] = renter
   end
+
   def charge(boat)
     bill = {}
     if boat.hours_rented > @max_rental_time
@@ -20,5 +22,17 @@ class Dock
     bill[:card_number] = renter.credit_card_number
     bill[:amount] = amount
     bill
+  end
+
+  def log_hour
+    @rental_log.keys.each do |boat|
+      boat.add_hour
+    end
+  end
+
+  def return(boat)
+    rental_revenue = charge(boat)[:amount]
+    @rental_log.delete(boat)
+    @revenue += rental_revenue
   end
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -9,6 +9,8 @@ class DockTest < Minitest::Test
     @kayak_1 = Boat.new(:kayak, 20)
     @kayak_2 = Boat.new(:kayak, 20)
     @sup_1 = Boat.new(:standup_paddle_board, 15)
+    @canoe = Boat.new(:canoe, 25)
+    @sup_2 = Boat.new(:standup_paddle_board, 15)
     @patrick = Renter.new("Patrick Star", "4242424242424242")
     @eugene = Renter.new("Eugene Crabs", "1313131313131313")
   end
@@ -60,33 +62,104 @@ class DockTest < Minitest::Test
       }
     assert_equal expected, @dock.charge(@sup_1)
   end
+
+  def test_it_logs_hours
+    @dock.rent(@kayak_1, @patrick)
+    @dock.rent(@kayak_2, @patrick)
+    @dock.log_hour
+    @dock.rent(@canoe, @patrick)
+    @dock.log_hour
+    assert_equal 2, @kayak_1.hours_rented
+    assert_equal 1, @canoe.hours_rented
+  end
+
+  def test_it_returns_boats
+    @dock.rent(@kayak_1, @patrick)
+    @dock.rent(@kayak_2, @patrick)
+    expected = {@kayak_1 => @patrick,
+                @kayak_2 => @patrick}
+    assert_equal expected, @dock.rental_log
+    @dock.return(@kayak_1)
+    expected_new = {@kayak_2 => @patrick}
+    assert_equal expected_new, @dock.rental_log
+  end
+
+  def test_it_gets_revenue_when_boat_is_returned
+    @dock.rent(@kayak_1, @patrick)
+    @dock.rent(@kayak_2, @patrick)
+    @dock.log_hour
+    @dock.rent(@canoe, @patrick)
+    @dock.log_hour
+    assert_equal 0, @dock.revenue
+    @dock.return(@kayak_1)
+    @dock.return(@kayak_2)
+    @dock.return(@canoe)
+    assert_equal 105, @dock.revenue
+  end
+
+  def test_it_does_not_generate_revenue_for_hours_over_max
+    @dock.rent(@kayak_1, @patrick)
+    @dock.rent(@kayak_2, @patrick)
+    @dock.log_hour
+    @dock.rent(@canoe, @patrick)
+    @dock.log_hour
+    @dock.return(@kayak_1)
+    @dock.return(@kayak_2)
+    @dock.return(@canoe)
+    @dock.rent(@sup_1, @eugene)
+    @dock.rent(@sup_2, @eugene)
+    @dock.log_hour
+    @dock.log_hour
+    @dock.log_hour
+    @dock.log_hour
+    @dock.log_hour
+    @dock.return(@sup_1)
+    @dock.return(@sup_2)
+    assert_equal 195, @dock.revenue
+  end
 end
 
-# pry(main)> kayak_1.add_hour
+
+# pry(main)> dock.rent(canoe, patrick)
 #
-# pry(main)> kayak_1.add_hour
+# # kayak_1, kayak_2, and canoe are rented an additional hour
+# pry(main)> dock.log_hour
 #
-# pry(main)> dock.charge(kayak_1)
-# # =>
-# # {
-# #   :card_number => "4242424242424242",
-# #   :amount => 40
-# # }
+# # Revenue should not be generated until boats are returned
+# pry(main)> dock.revenue
+# # => 0
 #
-# pry(main)> sup_1.add_hour
+# pry(main)> dock.return(kayak_1)
 #
-# pry(main)> sup_1.add_hour
+# pry(main)> dock.return(kayak_2)
 #
-# pry(main)> sup_1.add_hour
+# pry(main)> dock.return(canoe)
 #
-# # Any hours past the max rental time should not count
-# pry(main)> sup_1.add_hour
+# # Revenue thus far
+# pry(main)> dock.revenue
+# # => 105
 #
-# pry(main)> sup_1.add_hour
+# # Rent Boats out to a second Renter
+# pry(main)> dock.rent(sup_1, eugene)
 #
-# pry(main)> dock.charge(sup_1)
-# # =>
-# # {
-# #   :card_number => "1313131313131313",
-# #   :amount => 45
-# # }
+# pry(main)> dock.rent(sup_2, eugene)
+#
+# pry(main)> dock.log_hour
+#
+# pry(main)> dock.log_hour
+#
+# pry(main)> dock.log_hour
+#
+# # Any hours rented past the max rental time don't factor into revenue
+# pry(main)> dock.log_hour
+#
+# pry(main)> dock.log_hour
+#
+# pry(main)> dock.return(sup_1)
+#
+# pry(main)> dock.return(sup_2)
+#
+# # Total revenue
+# pry(main)> dock.revenue
+# # => 195
+# ```
